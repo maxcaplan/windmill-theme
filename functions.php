4cd4448edb93fbdb616d8c721558c3acf1a6ce7c
maxcaplan <maxacaplan@gmail.com>
2025-12-16 19:55:57 -0400

    Setup wordpress coding standard code sniffing

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Windmill Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package windmill-theme
+ * @author Max Caplan
+ * @license GNU General Public License v3
+ */
+
+namespace WindmillTheme;
